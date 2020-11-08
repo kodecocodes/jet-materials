eeedd49fdaefceba756ca7a05f24f09ee1f9aa04
dbuketa <denis.buketa@fiveminutes.eu>
2020-11-08 17:19:57 +0100

    Added color widget to note composable

--- 06-using-compose-modifiers/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/Note.kt
+++ 06-using-compose-modifiers/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/Note.kt
@@ -33,26 +33,26 @@
  */
 package com.raywenderlich.android.jetnotes.ui.components
 
-import androidx.compose.runtime.Composable
-import androidx.ui.tooling.preview.Preview
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Checkbox
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.raywenderlich.android.jetnotes.theme.rwGreen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.raywenderlich.android.jetnotes.theme.rwGreen
 
 @Composable
 fun Note() {
   Row(modifier = Modifier.fillMaxWidth()) {
-    Box(
-      modifier = Modifier.size(40.dp).background(rwGreen)
+    ColorWidget(
+      color = rwGreen,
+      size = 40.dp,
+      padding = 4.dp,
+      border = 1.dp
     )
     Column(modifier = Modifier.weight(1f)) {
       Text(text = "Title", maxLines = 1)
