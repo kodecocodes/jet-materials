48cfa309f63187fd8b45027066cb4356dfbfafa1
dbuketa <denis.buketa@fiveminutes.eu>
2020-11-08 15:52:36 +0100

    Added color widget

--- 06-using-compose-modifiers/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/ColorWidget.kt
+++ 06-using-compose-modifiers/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/ColorWidget.kt
@@ -33,26 +33,52 @@
  */
 package com.raywenderlich.android.jetnotes.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 @Composable
-fun ColorWidget() {
+fun ColorWidget(
+  color: Color,
+  size: Dp,
+  padding: Dp = 0.dp,
+  border: Dp
+) {
   Box(
     modifier = Modifier
-      .size(40.dp)
-      .background(Color.Red)
+      .size(size)
+      .padding(padding)
+      .clip(CircleShape)
+      .background(color)
+      .border(
+        BorderStroke(
+          border,
+          SolidColor(Color.Black)
+        ),
+        CircleShape
+      )
   )
 }
 
 @Preview
 @Composable
 fun ColorWidgetPreview() {
-  ColorWidget()
+  ColorWidget(
+    color = Color.Red,
+    size = 40.dp,
+    padding = 0.dp,
+    border = 2.dp
+  )
 }
