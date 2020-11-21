edc2b9bfca1d444d762db78f82ef6c282d3b20c6
Tino Balint <tino.balint@crionis.com>
2020-11-21 14:42:07 +0100

    adding projects

---
+++ 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/domain/model/PostType.kt
@@ -0,0 +1,18 @@
+package com.raywenderlich.android.jetreddit.domain.model
+
+enum class PostType(val type: Int) {
+
+  TEXT(0),
+  IMAGE(1);
+
+  companion object{
+
+    fun fromType(type: Int): PostType{
+      return if (type == TEXT.type){
+        TEXT
+      }else{
+        IMAGE
+      }
+    }
+  }
+}
---
+++ 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/screens/MyProfileScreen.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.android.jetreddit.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.raywenderlich.android.jetreddit.viewmodel.MainViewModel
+
+@Composable
+fun MyProfileScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+
+}
