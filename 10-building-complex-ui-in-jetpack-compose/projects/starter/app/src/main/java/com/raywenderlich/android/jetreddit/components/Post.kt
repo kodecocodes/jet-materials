fe1575b192da1e006e119ff57b03ee924dd23456
narko <6020peaks@gmail.com>
2020-12-10 21:37:55 +0100

    Adding missing imports

--- 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/components/Post.kt
+++ 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/components/Post.kt
@@ -37,12 +37,14 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
