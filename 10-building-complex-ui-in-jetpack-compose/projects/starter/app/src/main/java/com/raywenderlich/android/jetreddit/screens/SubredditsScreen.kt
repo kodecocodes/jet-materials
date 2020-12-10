45648d79569fdd322f5cde42a98f45d14350d0e2
narko <6020peaks@gmail.com>
2020-12-10 22:07:57 +0100

    TE: Adding imports to starter project

--- 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/screens/SubredditsScreen.kt
+++ 10-building-complex-ui-in-jetpack-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/screens/SubredditsScreen.kt
@@ -35,24 +35,32 @@ package com.raywenderlich.android.jetreddit.screens
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRowFor
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.raywenderlich.android.jetreddit.R
+import com.raywenderlich.android.jetreddit.components.BackgroundText
 import com.raywenderlich.android.jetreddit.models.SubredditModel
 
 val subreddits = listOf(
@@ -161,7 +169,7 @@ fun Community(text: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Communities(modifier: Modifier= Modifier){
+fun Communities(modifier: Modifier = Modifier){
     //TODO add your code here
 }
 
