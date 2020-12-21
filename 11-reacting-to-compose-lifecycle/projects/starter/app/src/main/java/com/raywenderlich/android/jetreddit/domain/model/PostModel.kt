aee35ca11b41ec695e9baf300d4ef9007503622d
Tino Balint <tino.balint@crionis.com>
2020-12-21 20:59:41 +0100

    adding projects

---
+++ 11-reacting-to-compose-lifecycle/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/domain/model/PostModel.kt
@@ -0,0 +1,21 @@
+package com.raywenderlich.android.jetreddit.domain.model
+
+data class PostModel(
+    val username: String,
+    val subreddit: String,
+    val title: String,
+    val text: String,
+    val likes: String,
+    val comments: String,
+    val type: PostType,
+    val postedTime: String
+){
+
+  companion object{
+
+    val DEFAULT_POST = PostModel("raywenderlich", "androiddev", "Check out this new book about Jetpack Compose from Raywenderlich!",
+        "Check out this new book about Jetpack Compose from Raywenderlich!", "5614", "523", PostType.TEXT, "4h")
+
+    val EMPTY = PostModel("raywenderlich","raywenderlich", "","", "0", "0", PostType.TEXT, "0h")
+  }
+}
---
+++ 11-reacting-to-compose-lifecycle/projects/starter/app/src/main/java/com/raywenderlich/android/jetreddit/screens/ChooseCommunityScreen.kt
@@ -0,0 +1,70 @@
+package com.raywenderlich.android.jetreddit.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.raywenderlich.android.jetreddit.R
+import com.raywenderlich.android.jetreddit.routing.JetRedditRouter
+import com.raywenderlich.android.jetreddit.viewmodel.MainViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+
+private const val SEARCH_DELAY_MILLIS = 300L
+
+private val defaultCommunities = listOf("raywenderlich", "androiddev", "puppies")
+
+@Composable
+fun ChooseCommunityScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+  //TODO Add your code here
+}
+
+@Composable
+fun SearchedCommunities(communities: List<String>, viewModel: MainViewModel?, modifier: Modifier = Modifier) {
+  //TODO Add your code here
+}
+
+@Composable
+fun ChooseCommunityTopBar(modifier: Modifier = Modifier) {
+
+  val colors = MaterialTheme.colors
+
+  TopAppBar(
+      title = {
+        Text(
+            fontSize = 16.sp,
+            text = stringResource(R.string.choose_community),
+            color = colors.primaryVariant
+        )
+      },
+      navigationIcon = {
+        IconButton(
+            onClick = { JetRedditRouter.goBack() }
+        ) {
+          Icon(
+              imageVector = Icons.Default.Close,
+              tint = colors.primaryVariant
+          )
+        }
+      },
+      backgroundColor = colors.primary,
+      elevation = 0.dp,
+      modifier = modifier
+          .preferredHeight(48.dp)
+          .background(Color.Blue)
+  )
+}
