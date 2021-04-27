e7ac697a49357c7c01ab6d663ccc5a014ee0cc0b
Filip Babic <fbabic96@gmail.com>
2021-04-27 11:52:39 +0200

    CH07 FPE Done

--- 07-managing-state-in-compose/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/NotesScreen.kt
+++ 07-managing-state-in-compose/projects/final/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/NotesScreen.kt
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2020 Razeware LLC
- * 
+ * Copyright (c) 2021 Razeware LLC
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  * distribute, sublicense, create a derivative work, and/or sell copies of the
  * Software in any work that is designed, intended, or marketed for pedagogical or
@@ -18,11 +18,11 @@
  * or information technology.  Permission for such use, copying, modification,
  * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
- * 
+ *
  * This project and source code may use libraries or frameworks that are
  * released under various Open-Source licenses. Use of those libraries and
  * frameworks are governed by their own individual licenses.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,11 +40,11 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.tooling.preview.Preview
 import com.raywenderlich.android.jetnotes.domain.model.NoteModel
 import com.raywenderlich.android.jetnotes.ui.components.Note
 import com.raywenderlich.android.jetnotes.ui.components.TopAppBar
 import com.raywenderlich.android.jetnotes.viewmodel.MainViewModel
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
--- 07-managing-state-in-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/AppDrawer.kt
+++ 07-managing-state-in-compose/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/AppDrawer.kt
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2020 Razeware LLC
- * 
+ * Copyright (c) 2021 Razeware LLC
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  * distribute, sublicense, create a derivative work, and/or sell copies of the
  * Software in any work that is designed, intended, or marketed for pedagogical or
@@ -18,11 +18,11 @@
  * or information technology.  Permission for such use, copying, modification,
  * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
- * 
+ *
  * This project and source code may use libraries or frameworks that are
  * released under various Open-Source licenses. Use of those libraries and
  * frameworks are governed by their own individual licenses.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,61 +34,24 @@
 package com.raywenderlich.android.jetnotes.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.raywenderlich.android.jetnotes.theme.JetNotesTheme
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.material.Surface
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.Switch
-import com.raywenderlich.android.jetnotes.theme.JetNotesThemeSettings
-import androidx.compose.material.icons.filled.Delete
 import com.raywenderlich.android.jetnotes.routing.JetNotesRouter
 import com.raywenderlich.android.jetnotes.routing.Screen
-import androidx.compose.material.Divider
-
-@Composable
-fun AppDrawer(
-  currentScreen: Screen,
-  closeDrawerAction: () -> Unit
-) {
-  Column(modifier = Modifier.fillMaxSize()) {
-    AppDrawerHeader()
-    Divider(
-      color = MaterialTheme
-        .colors.onSurface.copy(alpha = .2f)
-    )
-    ScreenNavigationButton(
-      icon = Icons.Filled.Home,
-      label = "Notes",
-      isSelected = currentScreen == Screen.Notes,
-      onClick = {
-        JetNotesRouter.navigateTo(Screen.Notes)
-        closeDrawerAction()
-      }
-    )
-    ScreenNavigationButton(
-      icon = Icons.Filled.Delete,
-      label = "Trash",
-      isSelected = currentScreen == Screen.Trash,
-      onClick = {
-        JetNotesRouter.navigateTo(Screen.Trash)
-        closeDrawerAction()
-      }
-    )
-    LightDarkThemeItem()
-  }
-}
+import com.raywenderlich.android.jetnotes.theme.JetNotesTheme
+import com.raywenderlich.android.jetnotes.theme.JetNotesThemeSettings
 
 @Composable
 private fun AppDrawerHeader() {
@@ -105,6 +68,14 @@ private fun AppDrawerHeader() {
       modifier = Modifier
         .align(alignment = Alignment.CenterVertically)
     )
+  }
+}
+
+@Preview
+@Composable
+fun AppDrawerHeaderPreview() {
+  JetNotesTheme {
+    AppDrawerHeader()
   }
 }
 
@@ -141,14 +112,14 @@ private fun ScreenNavigationButton(
     colors.surface
   }
 
-  Surface(
+  Surface( // 1
     modifier = Modifier
       .fillMaxWidth()
       .padding(start = 8.dp, end = 8.dp, top = 8.dp),
     color = backgroundColor,
     shape = MaterialTheme.shapes.small
   ) {
-    Row(
+    Row( // 2
       horizontalArrangement = Arrangement.Start,
       verticalAlignment = Alignment.CenterVertically,
       modifier = Modifier
@@ -162,7 +133,7 @@ private fun ScreenNavigationButton(
         colorFilter = ColorFilter.tint(textColor),
         alpha = imageAlpha
       )
-      Spacer(Modifier.width(16.dp))
+      Spacer(Modifier.width(16.dp)) // 3
       Text(
         text = label,
         style = MaterialTheme.typography.body2,
@@ -170,6 +141,19 @@ private fun ScreenNavigationButton(
         modifier = Modifier.fillMaxWidth()
       )
     }
+  }
+}
+
+@Preview
+@Composable
+fun ScreenNavigationButtonPreview() {
+  JetNotesTheme {
+    ScreenNavigationButton(
+      icon = Icons.Filled.Home,
+      label = "Notes",
+      isSelected = true,
+      onClick = { }
+    )
   }
 }
 
@@ -185,7 +169,8 @@ private fun LightDarkThemeItem() {
       color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
       modifier = Modifier
         .weight(1f)
-        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+        .align(alignment = Alignment.CenterVertically)
     )
     Switch(
       checked = JetNotesThemeSettings.isDarkThemeEnabled,
@@ -199,34 +184,48 @@ private fun LightDarkThemeItem() {
 
 @Preview
 @Composable
-fun AppDrawerPreview() {
-  JetNotesTheme {
-    AppDrawer(Screen.Notes, {})
-  }
-}
-
-@Preview
-@Composable
-fun AppDrawerHeaderPreview() {
-  JetNotesTheme {
-    AppDrawerHeader()
-  }
-}
-
-@Preview
-@Composable
-fun ScreenNavigationButtonPreview() {
-  JetNotesTheme {
-    ScreenNavigationButton(
-      icon = Icons.Filled.Home, label = "Notes", isSelected = true, onClick = { }
-    )
-  }
-}
-
-@Preview
-@Composable
 fun LightDarkThemeItemPreview() {
   JetNotesTheme {
     LightDarkThemeItem()
+  }
+}
+
+@Composable
+fun AppDrawer(
+  currentScreen: Screen,
+  closeDrawerAction: () -> Unit
+) {
+  Column(modifier = Modifier.fillMaxSize()) {
+    AppDrawerHeader()
+
+    Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+
+    ScreenNavigationButton(
+      icon = Icons.Filled.Home,
+      label = "Notes",
+      isSelected = currentScreen == Screen.Notes,
+      onClick = {
+        JetNotesRouter.navigateTo(Screen.Notes)
+        closeDrawerAction()
+      }
+    )
+    ScreenNavigationButton(
+      icon = Icons.Filled.Delete,
+      label = "Trash",
+      isSelected = currentScreen == Screen.Trash,
+      onClick = {
+        JetNotesRouter.navigateTo(Screen.Trash)
+        closeDrawerAction()
+      }
+    )
+    LightDarkThemeItem()
+  }
+}
+
+@Preview
+@Composable
+fun AppDrawerPreview() {
+  JetNotesTheme {
+    AppDrawer(Screen.Notes, {})
   }
 }
