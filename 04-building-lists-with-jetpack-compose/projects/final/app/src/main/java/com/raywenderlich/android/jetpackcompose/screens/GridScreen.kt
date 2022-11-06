630f9571c39005f7f63775d3e8977c80ad7d249c
Prateek Prasad <leavethemails@gmail.com>
2022-11-06 12:04:57 +0530

    final project updates for 2nd edition

--- 04-building-lists-with-jetpack-compose/projects/final/app/src/main/java/com/raywenderlich/android/jetpackcompose/screens/GridScreen.kt
+++ 04-building-lists-with-jetpack-compose/projects/final/app/src/main/java/com/raywenderlich/android/jetpackcompose/screens/GridScreen.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Razeware LLC
+ * Copyright (c) 2022 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,21 +34,21 @@
 
 package com.raywenderlich.android.jetpackcompose.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -57,7 +57,6 @@ import com.raywenderlich.android.jetpackcompose.R
 import com.raywenderlich.android.jetpackcompose.router.BackButtonHandler
 import com.raywenderlich.android.jetpackcompose.router.JetFundamentalsRouter
 import com.raywenderlich.android.jetpackcompose.router.Screen
-import kotlin.math.ceil
 
 private val items = listOf(
   Icons.Filled.Check,
@@ -72,17 +71,14 @@ private val items = listOf(
   Icons.Filled.ThumbUp,
 )
 
-data class IconResource(val imageVector: ImageVector, val isVisible: Boolean)
-
-@ExperimentalFoundationApi
 @Composable
 fun GridScreen() {
   LazyVerticalGrid(
     modifier = Modifier.fillMaxSize(),
-    cells = GridCells.Fixed(3),
+    columns = GridCells.Fixed(3),
     content = {
-      items(items) { item ->
-        GridIcon(IconResource(item, true))
+      items(items.size) { index ->
+        GridIcon(items[index])
       }
     }
   )
@@ -93,70 +89,13 @@ fun GridScreen() {
 }
 
 @Composable
-fun GridView(columnCount: Int) {
-  val itemSize = items.size
-  val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
-  val gridItems = mutableListOf<List<IconResource>>()
-  var position = 0
-
-  for (i in 0 until rowCount) {
-    val rowItem = mutableListOf<IconResource>()
-    for (j in 0 until columnCount) {
-      if (position.inc() <= itemSize) {
-        rowItem.add(IconResource(items[position++], true))
-      }
-    }
-    // here
-    val itemsToFill = columnCount - rowItem.size
-
-    for (j in 0 until itemsToFill) {
-      rowItem.add(IconResource(Icons.Filled.Delete, false))
-    }
-    gridItems.add(rowItem)
-  }
-  // here
-  LazyColumn(modifier = Modifier.fillMaxSize()) {
-    items(gridItems) { items ->
-      RowItem(items)
-    }
-  }
-}
-
-@Composable
-fun RowItem(rowItems: List<IconResource>) {
-  Row {
-    for (element in rowItems)
-      GridIcon(element)
-  }
-}
-
-@Composable
-fun RowScope.GridIcon(iconResource: IconResource) {
-  val color = if (iconResource.isVisible)
-    colorResource(R.color.colorPrimary)
-  else Color.Transparent
-
+fun GridIcon(iconResource: ImageVector) {
   Icon(
-    imageVector = iconResource.imageVector,
-    tint = color,
+    imageVector = iconResource,
+    tint = colorResource(R.color.colorPrimary),
     contentDescription = stringResource(R.string.grid_icon),
     modifier = Modifier
-      .size(80.dp, 80.dp)
-      .weight(1f)
-  )
-}
-
-@Composable
-fun GridIcon(iconResource: IconResource) {
-  val color = if (iconResource.isVisible)
-    colorResource(R.color.colorPrimary)
-  else Color.Transparent
-
-  Icon(
-    imageVector = iconResource.imageVector,
-    tint = color,
-    contentDescription = stringResource(R.string.grid_icon),
-    modifier = Modifier
-      .size(80.dp, 80.dp)
+      .size(80.dp)
+      .padding(20.dp)
   )
 }
