7b23abaf3d1aab4b804fc0ff0f7da0d4cb409f89
Denis Buketa <denis.buketa@gmail.com>
2022-11-12 17:50:26 +0100

    07-managing-state-in-compose - Rebranded starter and final project - Added Splash screen

---
+++ 07-managing-state-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2022 Kodeco Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.yourcompany.android.jetnotes.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yourcompany.android.jetnotes.domain.model.ColorModel
+import com.yourcompany.android.jetnotes.ui.components.NoteColor
+import com.yourcompany.android.jetnotes.util.fromHex
+
+@Composable
+private fun ColorPicker(
+  colors: List<ColorModel>,
+  onColorSelect: (ColorModel) -> Unit
+) {
+  Column(modifier = Modifier.fillMaxWidth()) {
+    Text(
+      text = "Color picker",
+      fontSize = 18.sp,
+      fontWeight = FontWeight.Bold,
+      modifier = Modifier.padding(8.dp)
+    )
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+      items(colors.size) { itemIndex ->
+        val color = colors[itemIndex]
+        ColorItem(
+          color = color,
+          onColorSelect = onColorSelect
+        )
+      }
+    }
+  }
+}
+
+@Composable
+fun ColorItem(
+  color: ColorModel,
+  onColorSelect: (ColorModel) -> Unit
+) {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .clickable(
+        onClick = {
+          onColorSelect(color)
+        }
+      )
+  ) {
+    NoteColor(
+      modifier = Modifier.padding(10.dp),
+      color = Color.fromHex(color.hex),
+      size = 80.dp,
+      border = 2.dp
+    )
+    Text(
+      text = color.name,
+      fontSize = 22.sp,
+      modifier = Modifier
+        .padding(horizontal = 16.dp)
+        .align(Alignment.CenterVertically)
+    )
+  }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview() {
+  ColorItem(ColorModel.DEFAULT) {}
+}
+
+@Preview
+@Composable
+fun ColorPickerPreview() {
+  ColorPicker(
+    colors = listOf(
+      ColorModel.DEFAULT,
+      ColorModel.DEFAULT,
+      ColorModel.DEFAULT
+    )
+  ) { }
+}
