72921a5d0eb84b54c8a1aea3f8b81518809c865c
Denis Buketa <denis.buketa@gmail.com>
2022-11-25 08:39:22 +0100

    In chat activity reset theme in onCreate

--- 13-adding-view-compatibility/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/ChatActivity.kt
+++ 13-adding-view-compatibility/projects/starter/app/src/main/java/com/yourcompany/android/jetreddit/screens/ChatActivity.kt
@@ -3,6 +3,7 @@ package com.yourcompany.android.jetreddit.screens
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.android.jetreddit.R
 import com.yourcompany.android.jetreddit.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class ChatActivity : AppCompatActivity() {
   private lateinit var binding: ActivityChatBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    // Switch to AppTheme for displaying the activity
+    setTheme(R.style.Theme_JetReddit)
+
     super.onCreate(savedInstanceState)
     binding = ActivityChatBinding.inflate(layoutInflater)
     val view = binding.root
