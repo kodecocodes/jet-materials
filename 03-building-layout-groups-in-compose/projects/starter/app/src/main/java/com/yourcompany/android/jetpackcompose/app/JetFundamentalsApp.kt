5f7f71cbd266999a5de962f24daaf98dff965d7c
Prateek Prasad <leavethemails@gmail.com>
2022-12-08 20:55:44 +0530

    update splash & copyrights

---
+++ 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/app/JetFundamentalsApp.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Kodeco Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.yourcompany.android.jetpackcompose.app
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
+import com.yourcompany.android.jetpackcompose.router.Screen
+import com.yourcompany.android.jetpackcompose.screens.*
+
+@Composable
+fun JetFundamentalsApp() {
+  Surface(color = MaterialTheme.colors.background) {
+    Crossfade(targetState = JetFundamentalsRouter.currentScreen) { screenState ->
+      when (screenState.value) {
+        is Screen.Navigation -> NavigationScreen()
+        is Screen.Text -> TextScreen()
+        is Screen.TextField -> TextFieldScreen()
+        is Screen.Buttons -> ExploreButtonsScreen()
+        is Screen.ProgressIndicator -> ProgressIndicatorScreen()
+        is Screen.AlertDialog -> AlertDialogScreen()
+        is Screen.Row -> RowScreen()
+        is Screen.Column -> ColumnScreen()
+        is Screen.Box -> BoxScreen()
+        is Screen.Surface -> SurfaceScreen()
+        is Screen.Scaffold -> ScaffoldScreen()
+      }
+    }
+  }
+}
