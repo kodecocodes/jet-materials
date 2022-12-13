b17c522881c6706d2af8362417458726ed477998
Prateek Prasad <leavethemails@gmail.com>
2022-12-13 15:52:06 +0530

    cleanup

--- 14-ui-tests-in-jetpack-compose/projects/final/app/src/androidTest/java/com/yourcompany/android/jetreddit/JetRedditAppTest.kt
+++ 14-ui-tests-in-jetpack-compose/projects/final/app/src/androidTest/java/com/yourcompany/android/jetreddit/JetRedditAppTest.kt
@@ -1,30 +1,18 @@
 package com.yourcompany.android.jetreddit
 
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.yourcompany.android.jetreddit.components.ImagePost
-import com.yourcompany.android.jetreddit.components.Post
-import com.yourcompany.android.jetreddit.components.TextPost
-import com.yourcompany.android.jetreddit.domain.model.PostModel
 import com.yourcompany.android.jetreddit.factory.PostDataFactory
 import com.yourcompany.android.jetreddit.routing.Screen
 import com.yourcompany.android.jetreddit.util.Tags
-import com.yourcompany.android.jetreddit.viewmodel.MainViewModel
-import com.yourcompany.android.jetreddit.viewmodel.MainViewModelFactory
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 class JetRedditAppTest {
 
