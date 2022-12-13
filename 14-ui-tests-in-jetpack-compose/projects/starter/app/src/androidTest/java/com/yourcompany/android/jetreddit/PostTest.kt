321facaa3d01ac01bf5e62458a13db6bcd7762e9
Prateek Prasad <leavethemails@gmail.com>
2022-12-13 15:53:42 +0530

    cleanup

--- 14-ui-tests-in-jetpack-compose/projects/starter/app/src/androidTest/java/com/yourcompany/android/jetreddit/PostTest.kt
+++ 14-ui-tests-in-jetpack-compose/projects/starter/app/src/androidTest/java/com/yourcompany/android/jetreddit/PostTest.kt
@@ -1,23 +1,6 @@
 package com.yourcompany.android.jetreddit
 
-import androidx.activity.viewModels
-import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.yourcompany.android.jetreddit.components.ImagePost
-import com.yourcompany.android.jetreddit.components.Post
-import com.yourcompany.android.jetreddit.components.TextPost
-import com.yourcompany.android.jetreddit.domain.model.PostModel
-import com.yourcompany.android.jetreddit.util.Tags
-import com.yourcompany.android.jetreddit.viewmodel.MainViewModel
-import com.yourcompany.android.jetreddit.viewmodel.MainViewModelFactory
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 class PostTest {
 
