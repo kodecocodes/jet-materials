4c2844fe390a0a750d19ca5b35e665dcddb8f9ef
learnWithZak <learn.with.zak@gmail.com>
2023-05-29 05:53:05 +0200

    refactor: modify the TextField to OutlinedTextField

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextFieldScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextFieldScreen.kt
@@ -38,40 +38,57 @@ package com.yourcompany.android.jetpackcompose.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
 
 @Composable
 fun TextFieldScreen() {
-  Column(
-      modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center
-  ) {
-    MyTextField()
-  }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        MyTextField()
+    }
 
-  BackButtonHandler {
-    JetFundamentalsRouter.navigateTo(Screen.Navigation)
-  }
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
 }
 
 @Composable
 fun MyTextField() {
-  val textValue = remember { mutableStateOf("") }
+    val textValue = remember { mutableStateOf("") }
+    val primaryColor = colorResource(id = R.color.colorPrimary)
 
-    TextField(
+    OutlinedTextField(
         value = textValue.value,
         onValueChange = {
             textValue.value = it
         },
-        label = {}
+        label = {
+            Text(text = stringResource(id = R.string.email))
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = primaryColor,
+            focusedLabelColor = primaryColor,
+            cursorColor = primaryColor
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
     )
 }
