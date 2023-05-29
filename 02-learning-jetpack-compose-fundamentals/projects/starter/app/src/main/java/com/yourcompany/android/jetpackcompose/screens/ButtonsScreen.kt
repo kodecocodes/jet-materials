611dd4799c7a0caad3161dcdb79ceadac289f158
learnWithZak <learn.with.zak@gmail.com>
2023-05-29 06:22:39 +0200

    refactor: add a Button

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
@@ -34,43 +34,66 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
 
 @Composable
 fun ExploreButtonsScreen() {
-  Column(modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-    MyButton()
-    MyRadioGroup()
-    MyFloatingActionButton()
+        MyButton()
+        MyRadioGroup()
+        MyFloatingActionButton()
 
-    BackButtonHandler {
-      JetFundamentalsRouter.navigateTo(Screen.Navigation)
+        BackButtonHandler {
+            JetFundamentalsRouter.navigateTo(Screen.Navigation)
+        }
     }
-  }
 }
 
 @Composable
 fun MyButton() {
-  //TODO add your code here
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.colorPrimary)),
+        border = BorderStroke(
+            1.dp,
+            color = colorResource(id = R.color.colorPrimaryDark)
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.button_text),
+            color = Color.White
+        )
+    }
 }
 
 @Composable
 fun MyRadioGroup() {
-  //TODO add your code here
+    //TODO add your code here
 }
 
 @Composable
 fun MyFloatingActionButton() {
-  //TODO add your code here
+    //TODO add your code here
 }
