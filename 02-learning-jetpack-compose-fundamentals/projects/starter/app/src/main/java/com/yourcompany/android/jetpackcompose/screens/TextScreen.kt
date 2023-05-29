171745dd2d22f2ee93e11d4cb03997639ef05b0c
learnWithZak <learn.with.zak@gmail.com>
2023-05-29 05:10:53 +0200

    refactor: display a simple text

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextScreen.kt
@@ -37,9 +37,12 @@ package com.yourcompany.android.jetpackcompose.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -61,5 +64,5 @@ fun TextScreen() {
 
 @Composable
 fun MyText() {
-  //TODO add your code here
+  Text(text = stringResource(id = R.string.jetpack_compose))
 }
