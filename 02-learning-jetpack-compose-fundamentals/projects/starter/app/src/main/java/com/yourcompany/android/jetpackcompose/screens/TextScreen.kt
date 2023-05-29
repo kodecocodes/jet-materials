5cf36767b3225ef876ac4e49d120c897a173c639
learnWithZak <learn.with.zak@gmail.com>
2023-05-29 05:15:13 +0200

    refactor: change its style

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/TextScreen.kt
@@ -41,7 +41,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
@@ -49,20 +54,28 @@ import com.yourcompany.android.jetpackcompose.router.Screen
 
 @Composable
 fun TextScreen() {
-  Column(
-      modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center
-  ) {
-    MyText()
-  }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        MyText()
+    }
 
-  BackButtonHandler {
-    JetFundamentalsRouter.navigateTo(Screen.Navigation)
-  }
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
 }
 
 @Composable
 fun MyText() {
-  Text(text = stringResource(id = R.string.jetpack_compose))
+    Text(
+        text = stringResource(id = R.string.jetpack_compose),
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        color = colorResource(
+            id = R.color.colorPrimary
+        )
+    )
 }
