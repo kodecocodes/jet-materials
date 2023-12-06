9de26e0aa9a1cf955a8149ffcb4ecde7734fc951
alexforrester <aforresterwork@gmail.com>
2023-12-06 16:12:33 +0000

    Updates starter and final chapter 3 projects with Scaffold PaddingValues

--- 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/RowScreen.kt
+++ 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/RowScreen.kt
@@ -34,7 +34,15 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
@@ -52,6 +60,6 @@ fun RowScreen() {
 }
 
 @Composable
-fun MyRow() {
+fun MyRow(modifier: Modifier = Modifier) {
   //TODO write your code here
 }
--- 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/SurfaceScreen.kt
+++ 03-building-layout-groups-in-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/SurfaceScreen.kt
@@ -34,11 +34,18 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -55,7 +62,15 @@ fun SurfaceScreen(modifier: Modifier = Modifier) {
   }
 }
 
-@Composable
-fun MySurface(modifier: Modifier) {
-  //TODO write your code here
-}
+  @Composable
+  fun MySurface(modifier: Modifier) {
+    Surface(
+      modifier = modifier.size(100.dp), // 1
+      color = Color.LightGray, // 2
+      contentColor = colorResource(id = R.color.colorPrimary), // 2
+      elevation = 1.dp, // 3
+      border = BorderStroke(1.dp, Color.Black) // 4
+    ) {
+      MyColumn() // 5
+    }
+  }
